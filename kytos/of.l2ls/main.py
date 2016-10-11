b8bfcc4b44b615cb72311d6e5eb70b019b8bb449
Raphael Cobe <rmcobe@ncc.unesp.br>
2016-10-11 15:06:24 -0300

    Pep8

--- kytos/of.l2ls/main.py
+++ kytos/of.l2ls/main.py
@@ -2,26 +2,16 @@
 
 import logging
 
-from pyof.v0x01.common.action import ActionOutput
-from pyof.v0x01.common.phy_port import Port
-
-from pyof.v0x01.common.flow_match import Match
-from pyof.v0x01.common.flow_match import FlowWildCards
-
-from pyof.foundation.network_types import Ethernet
-
-from pyof.v0x01.controller2switch.flow_mod import FlowMod
-from pyof.v0x01.controller2switch.flow_mod import FlowModCommand
-from pyof.v0x01.controller2switch.flow_mod import FlowModFlags
-
-from pyof.v0x01.controller2switch.packet_out import PacketOut
-
 # TODO: This timeout should be setup on a config file from this napp
-from kyco.constants import FLOOD_TIMEOUT
 from kyco.core.events import KycoEvent
 from kyco.core.napps import KycoCoreNApp
 from kyco.utils import listen_to
-
+from pyof.foundation.network_types import Ethernet
+from pyof.v0x01.common.action import ActionOutput
+from pyof.v0x01.common.flow_match import Match
+from pyof.v0x01.common.phy_port import Port
+from pyof.v0x01.controller2switch.flow_mod import FlowMod, FlowModCommand
+from pyof.v0x01.controller2switch.packet_out import PacketOut
 
 log = logging.getLogger('KycoNApp')
 
@@ -72,15 +62,13 @@ class Main(KycoCoreNApp):
             flow_mod = FlowMod()
             flow_mod.command = FlowModCommand.OFPFC_ADD
             flow_mod.match = Match()
-            #flow_mod.match.wildcards -= FlowWildCards.OFPFW_DL_SRC
-            #flow_mod.match.wildcards -= FlowWildCards.OFPFW_DL_DST
-            #flow_mod.match.wildcards -= FlowWildCards.OFPFW_DL_TYPE
             flow_mod.match.dl_src = ethernet.source.value
             flow_mod.match.dl_dst = ethernet.destination.value
             flow_mod.match.dl_type = ethernet.type
             flow_mod.buffer_id = packet_in.buffer_id
             flow_mod.actions.append(ActionOutput(port=ports[0]))
-            event_out = KycoEvent(name='kytos/of.l2ls.messages.out.ofpt_flow_mod',
+            event_out = KycoEvent(name=('kytos/of.l2ls.messages.out.'
+                                        'ofpt_flow_mod'),
                                   content={'destination': event.source,
                                            'message': flow_mod})
         else:
@@ -90,7 +78,8 @@ class Main(KycoCoreNApp):
             packet_out.in_port = packet_in.in_port
 
             packet_out.actions.append(ActionOutput(port=Port.OFPP_FLOOD))
-            event_out = KycoEvent(name='kytos/of.l2ls.messages.out.ofpt_packet_out',
+            event_out = KycoEvent(name=('kytos/of.l2ls.messages.out.'
+                                        'ofpt_packet_out'),
                                   content={'destination': event.source,
                                            'message': packet_out})
 
