b50f541bf75d38f817bf0f7d3729ba9ea34c778d
Macartur Sousa <macartur.sc@gmail.com>
2016-12-06 16:30:58 -0200

    Fix style guide and update of.core/kytos.json
    Fix #86

--- kytos/of.core/main.py
+++ kytos/of.core/main.py
@@ -1,12 +1,7 @@
-"""This App is the responsible for the main OpenFlow basic operations."""
+"""App responsible for the main OpenFlow basic operations."""
 
 from logging import getLogger
 
-from kyco.core.events import KycoEvent
-from kyco.core.flow import Flow
-from kyco.core.napps import KycoCoreNApp
-from kyco.core.switch import Interface
-from kyco.utils import listen_to
 from pyof.v0x01.common.utils import new_message_from_header
 from pyof.v0x01.controller2switch.common import FlowStatsRequest
 from pyof.v0x01.controller2switch.features_request import FeaturesRequest
@@ -14,22 +9,25 @@ from pyof.v0x01.controller2switch.stats_request import StatsRequest, StatsTypes
 from pyof.v0x01.symmetric.echo_reply import EchoReply
 from pyof.v0x01.symmetric.hello import Hello
 
-log = getLogger('Kyco')
+from kyco.core.events import KycoEvent
+from kyco.core.flow import Flow
+from kyco.core.napps import KycoCoreNApp
+from kyco.core.switch import Interface
+from kyco.utils import listen_to
+
+log = getLogger('of.core')
 STATS_INTERVAL = 5
 
 
 class Main(KycoCoreNApp):
-    """Main class of KycoCoreNApp, responsible for the main OpenFlow basic
-    operations.
-
-    """
+    """Main class of KycoCoreNapp responsible to OpenFlow basic operations."""
 
     def setup(self):
-        """'Replaces' the 'init' method for the KycoApp subclass.
+        """App initialization (used instead of ``__init__``).
 
         The setup method is automatically called by the run method.
-        Users shouldn't call this method directly."""
-        # TODO: App information goes to app_name.json
+        Users shouldn't call this method directly.
+        """
         self.name = 'kytos/of.core'
         self.execute_as_loop(STATS_INTERVAL)
         self.controller.log_websocket.register_log(log)
@@ -38,12 +36,18 @@ class Main(KycoCoreNApp):
         """Method to be runned once on app 'start' or in a loop.
 
         The execute method is called by the run method of KycoNApp class.
-        Users shouldn't call this method directly."""
+        Users shouldn't call this method directly.
+        """
         for switch in self.controller.switches.values():
             self._update_flow_list(switch)
 
     def _update_flow_list(self, switch):
-        """Requests ofp_stats of the flow type"""
+        """Method responsible for request stats of flow to switches.
+
+        Args:
+            switch(:class:`~kyco.core.switch.Switch`):
+                target to send a stats request.
+        """
         body = FlowStatsRequest()  # Port.OFPP_NONE and All Tables
         req = StatsRequest(body_type=StatsTypes.OFPST_FLOW, body=body)
         req.pack()
@@ -54,8 +58,14 @@ class Main(KycoCoreNApp):
 
     @listen_to('kytos/of.core.messages.in.ofpt_stats_reply')
     def handle_flow_stats_reply(self, event):
-        """Handles FlowStatsReply and updates the switch list with its
-        flowstats"""
+        """Handle flow stats reply message.
+
+        This method updates the switches list with its Flow Stats.
+
+        Args:
+            event (:class:`~kyco.core.events.KycoEvent):
+                Event with ofpt_stats_reply in message.
+        """
         msg = event.content['message']
         if msg.body_type == StatsTypes.OFPST_FLOW:
             switch = event.source.switch
@@ -67,10 +77,10 @@ class Main(KycoCoreNApp):
 
     @listen_to('kytos/of.core.messages.in.ofpt_features_reply')
     def handle_features_reply(self, event):
-        """Handle received FeaturesReply event.
+        """Handle received kytos/of.core.messages.in.ofpt_features_reply event.
 
-        Reads the FeaturesReply Event sent by the client, save this data and
-        sends three new messages to the client:
+        Reads the KycoEvent with features reply message sent by the client,
+        save this data and sends three new messages to the client:
 
             * SetConfig Message;
             * FlowMod Message with a FlowDelete command;
@@ -79,9 +89,9 @@ class Main(KycoCoreNApp):
         This is the end of the Handshake workflow of the OpenFlow Protocol.
 
         Args:
-            event (KycoMessageInFeaturesReply):
+            event (KycoEvent): Event with features reply message.
         """
-        log.debug('Handling KycoMessageInFeaturesReply Event')
+        log.debug('Handling Features Reply Event')
 
         features = event.content['message']
         dpid = features.datapath_id.value
@@ -102,10 +112,10 @@ class Main(KycoCoreNApp):
 
     @listen_to('kyco/core.messages.openflow.new')
     def handle_new_openflow_message(self, event):
-        """Handle a RawEvent and generate a KycoMessageIn event.
+        """Handle a RawEvent and generate a kyco/core.messages.in.* event.
 
         Args:
-            event (KycoRawOpenFlowMessage): RawOpenFlowMessage to be unpacked
+            event (KycoEvent): RawEvent with openflow message to be unpacked
         """
         log.debug('RawOpenFlowMessage received by RawOFMessage handler')
 
@@ -128,10 +138,14 @@ class Main(KycoCoreNApp):
 
     @listen_to('kytos/of.core.messages.in.ofpt_echo_request')
     def handle_echo_request(self, event):
-        """Handle EchoRequest Event by Generating an EchoReply Answer
+        """Handle Echo Request Messages.
+
+        This method will get a echo request sent by client and generate a
+        echo reply as answer.
 
         Args:
-            event (KycoMessageInEchoRequest): Received Event
+            event (:class:`~kyco.core.events.KycoEvent`):
+                Event with echo request in message.
         """
         log.debug("Echo Request message read")
 
@@ -145,7 +159,10 @@ class Main(KycoCoreNApp):
 
     @listen_to('kytos/of.core.messages.in.ofpt_hello')
     def handle_openflow_in_hello(self, event):
-        """Handle a Hello MessageIn Event and sends a Hello to the client.
+        """Handle hello messages.
+
+        This method will get a KycoEvent with hello message sent by client and
+        sends a hello message to the client.
 
         Args:
             event (KycoMessageInHello): KycoMessageInHelloEvent
@@ -162,16 +179,17 @@ class Main(KycoCoreNApp):
     @listen_to('kytos/of.core.messages.out.ofpt_hello',
                'kytos/of.core.messages.out.ofpt_echo_reply')
     def send_features_request(self, event):
-        """Send a FeaturesRequest to the switch after a Hello Message.
+        """Send a feature request to the switch after a hello Message.
 
-        We consider here that the Hello is sent just during the Handshake
+        We consider here that the hello is sent just during the Handshake
         processes, which means that, at this point, we do not have the switch
         `dpid`, just the `connection`.
 
         Args:
-            event (KycoMessageOutHello): KycoMessageOutHello
+            event (:class:`~.kyco.core.events.KycoEvent`):
+                Event with hello message.
         """
-        log.debug('Sending a FeaturesRequest after responding to a Hello')
+        log.debug('Sending a feature request after responding to a hello')
 
         event_out = KycoEvent(name=('kytos/of.core.messages.out.'
                                     'ofpt_features_request'),
@@ -180,4 +198,5 @@ class Main(KycoCoreNApp):
         self.controller.buffers.msg_out.put(event_out)
 
     def shutdown(self):
+        """End of the application."""
         log.debug('Shutting down...')
