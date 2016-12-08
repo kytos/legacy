f57d89114dcaa958096dab2e79f8d307fbe0ae6f
Macartur Sousa <macartur.sc@gmail.com>
2016-12-08 15:27:26 -0200

    Improving web.topology.layout
    - Adding longdescrition in kytos.json
    - Fixing quick_lint warnings
    - Fix #94

--- kytos/web.topology.layout/main.py
+++ kytos/web.topology.layout/main.py
@@ -1,31 +1,35 @@
-import logging
-import json
+"""Module to manager the settings and layout of kyco web interface."""
 
-from flask import request
+import json
+import logging
 from os import listdir, makedirs
 from os.path import isfile, join
 
-from kyco.core.napps import KycoCoreNApp
+from flask import request
 
+from kyco.core.napps import KycoCoreNApp
 
 log = logging.getLogger('KycoNApp')
 
 TOPOLOGY_DIR = '/tmp/kytos/topologies/'
 makedirs(TOPOLOGY_DIR, exist_ok=True)
 
+
 class Main(KycoCoreNApp):
-    """
+    """Main class for Web Topology Layout application.
+
     This app intends to update the links between machines and switches. It
     considers that if an interface is connected to another interface then this
-    is a link. If not, it must be a connection to a server
+    is a link. If not, it must be a connection to a server.
     """
 
     def setup(self):
-        """Creates an empty dict to store the switches references and the
-        information about the hosts"""
+        """Method used to create the web.topology.layout routes.
 
+        This method will register the routes ['kytos/web/topology/layouts/',
+        'web/topology/layouts/<name>'] to save and recover topologies.
+        """
         self.name = 'kytos/web.topology.layout'
-        self.stop_signal = False
         self.current_controller = self.controller
         self.controller.register_rest_endpoint('/web/topology/layouts/',
                                                self.get_topologies,
@@ -35,19 +39,38 @@ class Main(KycoCoreNApp):
                                                methods=['GET', 'POST'])
 
     def execute(self):
-        """ Do nothing, only wait for packet-in messages"""
+        """Do nothing."""
         pass
 
     def shutdown(self):
-        self.stop_signal = True
+        """End of the application."""
+        log.debug('Shutting down...')
 
     def topology(self, name):
+        """Method used to save or load a topology layout.
+
+        Paramters:
+            name (string): mame of the topology to be saved or loaded.
+
+        Returns:
+            topology (string): topology using json format.
+        """
         if request.method == 'POST':
             return self.save_topology(name)
         else:
             return self.get_topology(name)
 
     def save_topology(self, name):
+        """Save a topology layout in a file.
+
+        This method get a json topology from request and puts this in a file.
+
+        Parameters:
+            name (string): name of the topology to  be saved or loaded.
+
+        Returns:
+            topology (string): topology using json format.
+        """
         if not request.is_json:
             return json.dumps('{"error": "gt was not a JSON request"}'), 400
         topology = request.get_json()
@@ -56,6 +79,14 @@ class Main(KycoCoreNApp):
         return json.dumps({'response': 'Saved'}), 201
 
     def get_topology(self, name):
+        """Method to read and return a topology from json file.
+
+        Paramters:
+            name (string): name of the topology to be saved or loaded.
+
+        Returns:
+            topology (string): topology using json format.
+        """
         file = join(TOPOLOGY_DIR, name + '.json')
         if not isfile(file):
             return json.dumps('{"error": "Topology not found"}'), 400
@@ -64,6 +95,15 @@ class Main(KycoCoreNApp):
         return json.dumps(output)
 
     def get_topologies(self):
+        """Method used to returns all topologies.
+
+        Returns:
+            topologies (string): all topologies saved using json format.
+        """
         files = listdir(TOPOLOGY_DIR)
-        output = [f.replace('.json', '') for f in files if isfile(join(TOPOLOGY_DIR, f))]
+        output = []
+        for f in files:
+            if isfile(join(TOPOLOGY_DIR, f)):
+                output.append(f.replace('.json', ''))
+
         return json.dumps(output)
