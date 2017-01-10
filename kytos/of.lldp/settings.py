2e05432e8f03c0cdd3c780e72aef81b913528d92
Artur Baruchi <artur.baruchi@gmail.com>
2017-01-10 12:50:15 -0200

    Adds settings.py file for all Napps  - Issue #150

---
+++ kytos/of.lldp/settings.py
@@ -0,0 +1,10 @@
+import logging
+
+# Log Registry Type
+log = logging.getLogger(__name__)
+
+# Ether type
+ether_type = 0x88cc
+
+# Mac Address used by LLDP to multicast messages
+lldp_multicast = '01:80:c2:00:00:0e'
---
+++ kytos/of.topology/settings.py
@@ -0,0 +1,7 @@
+import logging
+
+# Log Registry Type
+log = logging.getLogger('of.topology')
+
+# Ether type
+ether_type = 0x88cc
