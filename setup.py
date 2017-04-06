18e5d30bb76ddad5d7e709832c5ec201f5232371
Carlos Eduardo Moreira dos Santos <cadu@ncc.unesp.br>
2017-04-06 14:16:48 -0300

    Update version number

--- setup.py
+++ setup.py
@@ -185,7 +185,7 @@ class DevelopMode(develop):
 requirements = [i.strip() for i in open("requirements.txt").readlines()]
 
 setup(name='kytos-napps',
-      version='1.1.0b1.dev1',
+      version='2017.1b1',
       description='Core Napps developed by Kytos Team',
       url='http://github.com/kytos/kytos-napps',
       author='Kytos Team',
