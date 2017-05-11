209cf06c0ac21d35e86f63cd32669bb57d8e9fbb
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2017-05-11 13:06:24 -0300

    Ignoring unkown LLDP packets.
    fix #230

--- napps/kytos/of_lldp/settings.py
+++ napps/kytos/of_lldp/settings.py
@@ -1,2 +1,8 @@
 """Settings for the of_lldp NApp."""
 POOLING_TIME = 3
+
+# If we receive LLDP Packets that does not fit our format/needs, we will ignore
+# it. But it can be logged for trace/debug purposes. Here we need to define the
+# logLevel we want for this packets as a string (DEBUG, INFO, WARNING, etc) or
+# None (python None type)
+UNKOWN_LLDP_PACKETS_LOG_LEVEL = 'WARNING'
