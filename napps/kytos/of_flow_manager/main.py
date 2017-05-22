7affb918775ba41b909bce48b5ef41357a9390b9
Renan Rodrigo <renanrb@ime.usp.br>
2017-05-22 10:20:58 -0300

    Refactor of_flow_manager REST endpoints
    Fix #241
    Change the REST endpoints to standardize and simplify, fix the DELETE endpoint to work properly when clearing flows.

--- napps/kytos/of_flow_manager/main.py
+++ napps/kytos/of_flow_manager/main.py
@@ -19,16 +19,21 @@ class Main(KytosNApp):
         The setup method is automatically called by the run method.
         Users shouldn't call this method directly.
         """
-        self.execute_as_loop(settings.STATS_INTERVAL)
         self.flow_manager = FlowManager(self.controller)
-        endpoints = [('/flow-manager/<dpid>/flows', self.retrieve_flows,
+        endpoints = [('/flow-manager/flows', self.retrieve_flows,
                       ['GET']),
-                     ('/flow-manager/flows', self.retrieve_flows,
+                     ('/flow-manager/flows/<dpid>', self.retrieve_flows,
                       ['GET']),
-                     ('/flow-manager/<dpid>/flows-a', self.insert_flow,
+                     ('/flow-manager/flows', self.insert_flows,
                       ['POST']),
-                     ('/flow-manager/<dpid>/<flow_id>/flows-d',
-                      self.delete_flow, ['DELETE'])]
+                     ('/flow-manager/flows/<dpid>', self.insert_flows,
+                      ['POST']),
+                     ('/flow-manager/flows', self.delete_flows,
+                      ['DELETE']),
+                     ('/flow-manager/flows/<dpid>', self.delete_flows,
+                      ['DELETE']),
+                     ('/flow-manager/flows/<dpid>/<flow_id>',
+                      self.delete_flows, ['DELETE'])]
         for endpoint in endpoints:
             self.controller.register_rest_endpoint(*endpoint)
 
@@ -45,67 +50,59 @@ class Main(KytosNApp):
         log.debug("flow-manager stopping")
 
     def retrieve_flows(self, dpid=None):
-        """Retrieve all flows from a sitch identified by dpid.
+        """Retrieve all flows from a switch identified by dpid.
 
         If no dpid has been specified, returns the flows from all switches.
         """
         switch_flows = {}
-        if dpid is not None:
+        if dpid:
             switch = self.controller.get_switch_by_dpid(dpid)
             flows = []
             for flow in switch.flows:
-                flows.append(flow.as_dict())
+                flows.append(flow.as_dict()['flow'])
             switch_flows[dpid] = flows
         else:
             for switch_dpid in self.controller.switches:
                 switch = self.controller.get_switch_by_dpid(switch_dpid)
                 flows = []
                 for flow in switch.flows:
-                    flows.append(flow.as_dict())
+                    flows.append(flow.as_dict()['flow'])
                 switch_flows[switch_dpid] = flows
         return json.dumps(switch_flows)
 
-    def insert_flow(self, dpid=None):
-        """Insert a new flow to the switch identified by dpid.
+    def insert_flows(self, dpid=None):
+        """Install new flows in the switch identified by dpid.
 
-        If no dpid has been specified, install flow in all switches.
+        If no dpid has been specified, install flows in all switches.
         """
         json_content = request.get_json()
-        for json_flow in json_content['flows']:
+        for json_flow in json_content:
             received_flow = Flow.from_dict(json_flow)
-            if dpid is not None:
+            if dpid:
                 self.flow_manager.install_new_flow(received_flow, dpid)
             else:
                 for switch_dpid in self.controller.switches:
                     self.flow_manager.install_new_flow(received_flow,
                                                        switch_dpid)
 
-        return json.dumps({"response": "Flow Created"}), 201
+        return json.dumps({"response": "FlowMod Messages Sent"}), 201
 
-    def clear_flows(self, dpid=None):
-        """Clear flows from a switch identified by dpid.
+    def delete_flows(self, flow_id=None, dpid=None):
+        """Delete a flow from a switch identified by flow_id and dpid.
 
-        If no dpid has been specified, clear all flows from all switches.
+        If no flow_id has been specified, removes all flows from the switch.
+        If no dpid or flow_id  has been specified, removes all flows from all
+        switches.
         """
-        if dpid is not None:
+        if flow_id:
+            self.flow_manager.delete_flow(flow_id, dpid)
+        elif dpid:
             self.flow_manager.clear_flows(dpid)
         else:
             for switch_dpid in self.controller.switches:
                 self.flow_manager.clear_flows(switch_dpid)
 
-    def delete_flow(self, flow_id, dpid=None):
-        """Delete a flow from a switch identified by flow_id and dpid.
-
-        If no dpid has been specified, removes all flows with the given flow_id
-        from all switches.
-        """
-        if dpid is not None:
-            self.flow_manager.delete_flow(flow_id, dpid)
-        else:
-            for switch_dpid in self.controller.switches:
-                self.flow_manager.delete_flow(flow_id, switch_dpid)
-
-        return json.dumps({"response": "Flow Removed"}), 202
+        return json.dumps({"response": "FlowMod Messages Sent"}), 202
 
 
 class FlowManager(object):
